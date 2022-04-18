ae368bfe42d4f29add0fd795a7654603206ebf5d
Mahendra Chauhan <mikesch803@gmail.com>
2022-04-18 10:48:09 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 export function Header() {
   return (
     <div className="navbar">
-      <h1 className="navbar-title">Quiz</h1>
+      <Link to="/">
+        <h1 className="navbar-title">Quiz</h1>
+      </Link>
       <div className="navbar-icons">
         <button className="btn btn-link">LOGIN</button>
       </div>
